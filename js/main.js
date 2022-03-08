3a4b80126a250842db266081d749db008d0b2372
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-03-07 20:12:28 -0500

    added new grid for next tetromino

--- js/main.js
+++ js/main.js
@@ -136,5 +136,31 @@ function rotate() {
   draw()
 }
 
+//show up-next tetromino in mini-grid display
+const displaySquares = document.querySelectorAll(".mini-grid div")
+const displayWidth = 4
+let displayIndex = 0
+
+// the Tetrominos without rotations
+const upNextTetrominoes = [
+  [1, displayWidth+1, displayWidth*2+1, 2] //lTetromino
+  [0, displayWidth, displayWidth+1, displayWidth*2+1] //ZTetromino
+  [1, displayWidth, displayWidth+1, displayWidth+2] //tTetromino
+  [0, 1, displayWidth, displayWidth+1] //oTetromino
+  [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+]
+
+//display the shape in the mini-grid display
+function displayShape() {
+//remove any trace of a tetromino form the entire grid
+  displaySquares.forEach(square => {
+    square.classList.remove("tetromino")
+  })
+  upNextTetrominoes[nextRandom].forEach( index => {
+    displaySquares[displayIndex + index].classList.add('tetromino')
+  })
+}
+
+
 
 })
